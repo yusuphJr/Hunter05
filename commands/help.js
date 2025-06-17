eb6f39097f61949aec8d68d635c37bdc55bccbeb
yusuphJr <yusuphnandonde44@gmail.com>
2025-06-18 00:33:02 +0300

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,10 @@
+exports.run = async (msg, args, client) => {
+    msg.reply(`
+*Anonymous Bot Menu*
+- !gpt <your prompt>
+- !say <text>
+- !weather <city>
+- !news <type>
+- !img <keywords>
+    `);
+};
